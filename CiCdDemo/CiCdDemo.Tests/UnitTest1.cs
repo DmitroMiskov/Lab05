134aecd88960363b09062df5ff93de1ecc328c1a
Dmytro Miskov <dima.miskov04@gmail.com>
2025-11-30 14:17:34 +0200

    Initial commit

---
+++ CiCdDemo/CiCdDemo.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using CiCdDemo;
+
+namespace CiCdDemo.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_ReturnsCorrectResult()
+        {
+            var calc = new Calculator();
+            Assert.Equal(5, calc.Add(2, 3));
+        }
+
+        [Fact]
+        public void Subtract_ReturnsCorrectResult()
+        {
+            var calc = new Calculator();
+            Assert.Equal(1, calc.Subtract(3, 2));
+        }
+    }
+}
---
+++ CiCdDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CiCdDemo
+{
+    public class Calculator
+    {
+        public int Add(int a, int b) => a + b;
+        public int Subtract(int a, int b) => a - b;
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("CI/CD Demo running...");
+        }
+    }
+}
